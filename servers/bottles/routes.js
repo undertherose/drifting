6b0987140b894543697d464c1c815e3761cbc030
chiuy5 <chiuy5@uw.edu>
2019-02-27 17:21:38 -0800

    added code for creating an ocean

--- servers/bottles/routes.js
+++ servers/bottles/routes.js
@@ -1,10 +1,33 @@
 const express = require('express');
-const Courses = require('./models/bottle');
+const Oceans = require('./models/ocean');
 
 const router = express.Router();
 const fetch = require("node-fetch");
 
 ////////////////////////// /bottles ENDPOINT ROUTERS //////////////////////////
+
+//create an ocean
+router.post("/ocean", (req, res) => {
+    //let user = JSON.parse(req.get("X-User"));
+    
+    Oceans.create({
+        name: req.body.name
+    }).then(ocean => {
+        //insert rabbitMQ stuff
+        ocean.save().then(() => {
+            res.setHeader("Content-Type", "application/json");
+            res.status(201).send(ocean);
+        }).catch(err => {
+            console.log(err);
+        });
+
+    }).catch(err => {
+        console.log(err);
+    });
+}).catch(err => {
+    res.status(400).send({error : "couldn't create a ocean " + err});
+});
+
 
 
 
